a820019b1c480e20861df240054d2c1e4c69a247
elle <svilena_koleva@yahoo.com>
2016-09-23 17:49:17 +0200

    hyperlinks
    Added

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@ font-size: 13px;
 	
 </style>
 <div id="footer">
-<p>Made by Svilena Koleva [cph-sk187] CPH Business Project &copy; 2016</p>
+<p>Made by <a style="text-decoration:none;" href="www.creartivology.com" target="new">Svilena Koleva</a> [cph-sk187] <a style="text-decoration:none;" href="www.cphbusiness.dk">CPH Business</a> Project &copy; 2016</p>
 </div>
 </body>
 </html>
