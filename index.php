8ff4fd91a7d0936fa813747a3ce41332b8a70a46
elle <svilena_koleva@yahoo.com>
2016-09-23 18:11:35 +0200

    link
    added to otiginal post

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 <?php include 'menu.php';?>
 <br>
 <h1 style="text-align:center; padding: 5px;">Target Branding by Allan Peters</h1>
+<br>
+<a style="text-align:center; padding: 43%; text-decoration:none; color:#DE022A;" href="https://www.behance.net/gallery/31826275/2015-Target-Branding?" target="new">Link to original post >></a>
 
   <article class="article">
   <img src="img/buble.png" alt="buble" style="width:100%;height:100%;">
