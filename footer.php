81ab86571c447eb50f74dbfad7885289c3c7dacb
elle <svilena_koleva@yahoo.com>
2016-09-23 14:29:13 +0200

    footer style
    updating the footer style

--- footer.php
+++ footer.php
@@ -1,5 +1,22 @@
+<style>
+#footer {
+background-color: black;
+align-content: center;
+text-align:center;
+height: 50px;
+padding: 0.5%;
+}
 
-<p>Footer content here - phone: 11 22 33 44   &copy; 2016</p>
+p { 
+color: white;
+font-family: verdana;
+font-size: 13px;
+}
+	
+</style>
+<div id="footer">
+<p>Made by Svilena Koleva [cph-sk187]  &copy; 2016</p>
+</div>
 </body>
 </html>
 
